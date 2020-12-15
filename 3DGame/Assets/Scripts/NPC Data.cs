50448ca21cd6bca3a4b67aeed99d0f312d15139b
jerry411002 <71679892+jerry411002@users.noreply.github.com>
2020-12-15 16:02:00 +0800

    1

---
+++ 3DGame/Assets/Scripts/NPC Data.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+//建立資源選單("檔案名稱","選單名稱")
+[CreateAssetMenu(fileName ="NPC資訊",menuName ="IceTalker/NPC資料")]
+public class NPCData : ScriptableObject
+    //scriptable;腳本化物件
+    //將腳本資料變成物件保存在專案內
+{
+    [Header("第一段對話"),TextArea(1,5)]
+    public string dialougA; 
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialougB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialougC;
+
+    [Header("任務項目需求數量")]
+    public int count; 
+    [Header("已取得項目需求數量")]
+    public int countCurrent;
+        
+
+}
---
+++ 3DGame/Assets/Scripts/NPC.cs
@@ -0,0 +1,54 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData Data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+
+
+    ///<summary>
+    ///玩家是否進入感應區
+    ///</summary>
+
+    public bool playerInarea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "阿明")
+        {
+            playerInarea = true;
+            dialoug();
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "阿明")
+        { playerInarea = false; }
+    }
+
+    private void dialoug()
+    {
+        //print(Data.dialougA);
+        //rint(Data.dialougA[3]);   //娶得字串部份資料:語法[編號]
+
+        //ctrl+c+k 加註解
+        //for 迴圈：重複處理相同程式
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    print("我是迴圈：" + i);
+
+        //}
+        for (int i = 0; i <Data.dialougA.Length;i++)
+        {
+            print(Data.dialougA[i]);
+
+        }
+    }    
+}
